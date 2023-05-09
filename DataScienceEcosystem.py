88399f46fffddfb467d06190e4bd2db26daa7a23
Sai Hanuman <85697547+Saihanuman-17@users.noreply.github.com>
2023-05-09 20:45:58 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Data Science Tools and Ecosystem</h1>
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# <b>Objectives:</b>
+# ->Python
+# ->R
+# ->SQL
+# ->Java
+# ->Julia
+# ->Scala
+
+# Some of the popular languages that Data Scientists use are:
+# 1.Python
+# 2.SQL
+# 3.Java
+# 4.Scala
+# 5.Matlab
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.pandas
+# 2.numpy
+# 3.matplotlib
+# 4.tensorflow
+# 5.Scipy
+# 6.Scikit-learn
+
+# | Data Science Tools |
+# |--------------------|
+# |RStudio|
+# |Apache Spark|
+# |TensorFlow|
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# <h2>Author</h2>
+# Sai Hanuman
+
+# In[ ]:
+
+
+
+
